9beef26eb568575c0b425265a98604dd80fff8ff
mendesg0 <gabriel@prestige.com.br>
2020-10-15 17:57:28 -0300

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,12 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Dimensions, StyleSheet, Text, View } from "react-native";
+import MapView, { Callout, Marker, PROVIDER_GOOGLE } from "react-native-maps";
+
+import mapMarker from "./src/assets/map-marker.png";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        style={styles.map}
+        initialRegion={{
+          latitude: -23.7659865,
+          longitude: -53.2934951,
+          latitudeDelta: 0.008,
+          longitudeDelta: 0.008,
+        }}
+      >
+        <Marker
+          icon={mapMarker}
+          coordinate={{ latitude: -23.7659865, longitude: -53.2934951 }}
+        >
+          <Callout>
+            <Text>Lar das meninas</Text>
+          </Callout>
+        </Marker>
+      </MapView>
     </View>
   );
 }
@@ -14,8 +33,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  map: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
   },
 });
