e5af64db324828604726b39cbf96ef506e6082a7
gmendes0 <gmendes230@gmail.com>
2020-10-15 22:58:27 -0300

    feat: map page

--- App.tsx
+++ App.tsx
@@ -1,41 +1,22 @@
 import React from "react";
-import { Dimensions, StyleSheet, Text, View } from "react-native";
-import MapView, { Callout, Marker, PROVIDER_GOOGLE } from "react-native-maps";
 
-import mapMarker from "./src/assets/map-marker.png";
+import { useFonts } from "expo-font";
+import {
+  Nunito_600SemiBold,
+  Nunito_700Bold,
+  Nunito_800ExtraBold,
+} from "@expo-google-fonts/nunito";
+
+import Routes from "./src/routes";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <MapView
-        provider={PROVIDER_GOOGLE}
-        style={styles.map}
-        initialRegion={{
-          latitude: -23.7659865,
-          longitude: -53.2934951,
-          latitudeDelta: 0.008,
-          longitudeDelta: 0.008,
-        }}
-      >
-        <Marker
-          icon={mapMarker}
-          coordinate={{ latitude: -23.7659865, longitude: -53.2934951 }}
-        >
-          <Callout>
-            <Text>Lar das meninas</Text>
-          </Callout>
-        </Marker>
-      </MapView>
-    </View>
-  );
-}
+  const [fontsLoaded] = useFonts({
+    Nunito_600SemiBold,
+    Nunito_700Bold,
+    Nunito_800ExtraBold,
+  });
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  map: {
-    width: Dimensions.get("window").width,
-    height: Dimensions.get("window").height,
-  },
-});
+  if (!fontsLoaded) return null;
+
+  return <Routes />;
+}
